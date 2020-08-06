af12b891e91c6c9d3217afe597c07638e56616a9
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 11:13:31 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,10 +22,13 @@ def third_challenge
         :age => "50"
         },
       :matriarch => {
-        :
+        :name => "Lady Capulet"
+        :age => "51"
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet"
+        :age => "15"
+        :status => "alive"
         },
       :heroine_friends => []
    }
