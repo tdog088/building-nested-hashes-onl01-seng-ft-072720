1a75e681fe74622f2b6ddb141b6961d6b9256818
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 11:31:30 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,9 +6,9 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
+          name: "Benvolio",
+          age: "17"
+          attitude: "worried"
         },
         {
           name: "Mercutio",
